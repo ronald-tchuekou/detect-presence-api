c5e23142496b0e649ebb5850e903af3723a08b26
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-05-28 11:30:29 +0100

    Configuration de la base de données.

--- api/config/database.conf.js
+++ api/config/database.conf.js
@@ -6,10 +6,10 @@ const knex = require('knex')
 exports.getDBInstance = () => knex({
    client: 'mysql',
    connection: {
-      host: '127.0.0.1',
+      host: 'eu-cdbr-west-02.cleardb.net',
       port: 3306,
-      user: 'root',
-      password: '',
-      database: "detect_presences"
+      user: 'b6fd8f95cd3521',
+      password: '5c965d14',
+      database: "heroku_dc1a5f9ebf1faef"
    }
 })
