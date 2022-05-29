ef9cebea9cfab12328be667950d29ddd3203c406
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-05-29 15:59:49 +0100

    Ajout des routes pour la modification des mots de passes oubliées.

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -5,7 +5,7 @@ const config = require('../config/auth.config')
 const moment = require("moment");
 
 exports.login = async (req, res) => {
-   try{
+   try {
       const response = await PersonnelModel.getUser({email: req.body.email})
       if (response.length === 0)
          return res.status(400).send({
@@ -19,7 +19,7 @@ exports.login = async (req, res) => {
             message: 'Votre mot de passe est incorrect !',
             error: passwordIsValid
          })
-      const pattern = { id: user.personnel_id, role: user.role }
+      const pattern = {id: user.personnel_id, role: user.role}
       jwt.sign(pattern, config.secret, {}, async (err, token) => {
             if (err) {
                return res.status(400).json({
@@ -37,7 +37,38 @@ exports.login = async (req, res) => {
             })
          }
       )
-   }catch(error){
+   } catch (error) {
+      res.status(400).json({
+         message: 'Une erreur lors de la connexion!',
+         error
+      })
+   }
+}
+
+exports.checkUserEmail = async (req, res) => {
+   try {
+      const response = await PersonnelModel.getUser({email: req.body.email})
+      if (response.length === 0)
+         return res.status(400).send({
+            message: 'Aucun personnel ne possède cette adresse e-mail !',
+            error: response
+         })
+      res.json(response[0])
+   } catch (error) {
+      res.status(400).json({
+         message: 'Une erreur lors de la connexion!',
+         error
+      })
+   }
+}
+
+exports.resetPassword = async (req, res) => {
+   try {
+      const response = await PersonnelModel.updateCycle({
+         password: bcrypt.hashSync(req.body.password, 8),
+      }, req.body.user_id)
+      res.json(response)
+   } catch (error) {
       res.status(400).json({
          message: 'Une erreur lors de la connexion!',
          error
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/auth.contoller");
 const router = express.Router()
 
 router.post("/login", controller.login);
+router.get('/pass-forgot', controller.checkUserEmail)
+router.put('/reset-password', controller.resetPassword)
 
 module.exports = router
